4e0580249cc6b3fb0e088fb98b887d21b8510354
stephanesdr <stadlerstephane@gmail.com>
2023-03-18 11:57:37 +0100

    add config tailwind with google font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Jost } from 'next/font/google'
+
+const jost = Jost({ subsets: ['latin'], variable: '--font-jost' })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={`${jost.variable} bg-slate-300 font-sans text-black`}>
+    <Component {...pageProps} />
+  </main>
 }
