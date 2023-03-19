da3a6ebca960507ec9380ab4bc585a85edacdf4d
stephanesdr <stadlerstephane@gmail.com>
2023-03-19 19:22:21 +0100

    add theme.ts config for colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { Tokens } from '../../src/styles/theme'
+
+const theme = extendTheme({
+  colors: {
+    green: {
+      ...Tokens.green.shades,
+    }
+  }
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <main>
-           <Component {...pageProps} />
-          </main>
+  return <ChakraProvider theme={theme}>
+             <Component {...pageProps} />
+         </ChakraProvider>
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+export const Tokens = {
+    "green": {
+        "base": "#87E0A0",
+        "shades": {
+            "50": "#E3FFEB",
+            "100": "#C1FFD3",
+            "200": "#A8F6BE",
+            "300": "#87E0A0",
+        },
+    },
+    "blue": {
+        "base": "#7CB6F1",
+        "shades": {
+            "50": "#E5F2FF",
+            "100": "#BBDDFF",
+            "200": "#97C5F4",
+            "300": "#7CB6F1",
+        },
+    },
+}
+
+export default Tokens;
